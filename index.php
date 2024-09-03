815913dc6d90fa84aedf591ad721de08999d9aea
Francescosommellaa <franc.sommella@gmail.com>
2024-09-03 15:58:34 +0200

    created list with movies the description the director and genre array

--- index.php
+++ index.php
@@ -1,21 +1,33 @@
 <?php
 // importare classi
-require_once __DIR__ . '/models/Movie.php';
 require_once __DIR__ . '/data/db.php';
+require_once __DIR__ . '/Model/Movie.php';
 ?>
 
 
 <!DOCTYPE html>
 <html lang="en">
+<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap-grid.css' integrity='sha512-hhSu9overYjKfSjPCtJW3688VHkfBh+W1pR5Mysll91bOJwGjYntytGTtVXb2aisFOaYXXDrO38NKXDRPJWu7A==' crossorigin='anonymous' />
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>php-oop-1</title>
 
-    <ul>
-        <li><?php echo $Movie->getInfo() ?></li>
-    </ul>
+    <div class="container my-5">
+        <h1>Lista film:</h1>
+
+        <div class="d-flex">
+            <?php foreach ($db as $movie): ?>
+                <div class="card" style="width: 18rem;">
+                    <div class="card-body">
+                        <p class="card-title"><?php echo $movie->getinfo()  ?></p>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+
+        </div>
+    </div>
 </head>
 
 <body>
